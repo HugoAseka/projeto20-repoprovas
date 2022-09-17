a64249059e77dbc78e18d723886cba71c0e298eb
HugoAseka <hugomaseka@gmail.com>
2022-09-16 22:06:39 -0300

    thunder-collection

--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -2,24 +2,31 @@ import app from "../src/index";
 import supertest from "supertest";
 import { prisma } from "../src/config/database";
 import { validTest } from "./factories/testFactory";
-import { generateNewUserData, generateUserData, createUser } from "./factories/userFactory";
+import {
+  generateNewUserData,
+  generateUserData,
+  createUser,
+} from "./factories/userFactory";
 
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE users;`;
   await prisma.$executeRaw`TRUNCATE TABLE tests;`;
 });
 
-describe("Tests POST /tests ", () => {
+describe("Tests POST /testes ", () => {
   it("Returns 401 with invalid token", async () => {
     const createdUser = await createUser();
     const responseLogin = await supertest(app).post("/login").send(createdUser);
-    console.log(responseLogin.body.token)
+    console.log(responseLogin.body.token, "token");
+    const token = responseLogin.body.token;
     const result = await supertest(app)
-    .post("/testes")
-    .send(validTest)
-    .set("Authorization", `Bearer ${responseLogin.body.token}`)
-    .set("Accept","application/json")
-    .expect(200)
-
+      .post("/testes")
+      .set("Authorization", `Bearer ${token}`)
+      .send(validTest);
+    expect(result.status).toEqual(200);
   });
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
 });
