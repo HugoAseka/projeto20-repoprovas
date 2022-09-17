29d0b3912345200aaa7eb2c2be3ba9c21754e1b7
HugoAseka <hugomaseka@gmail.com>
2022-09-17 10:37:01 -0300

    tests: get by disciplines with invalid token

--- tests/getByDiscipline.test.ts
+++ tests/getByDiscipline.test.ts
@@ -3,13 +3,18 @@ import app from "../src/index";
 import generateToken from "./factories/tokenFactory";
 
 const api = supertest(app);
-
+const route = "/testes/disciplinas"
 describe("Tests GET /testes/disciplinas  ", () => {
     it("return status code 200 and an array when requesting with valid token", async () => {
         const token = await generateToken();
-        const result  = await api.get("/testes/disciplinas").set("Authorization", `Bearer ${token}`);
+        const result  = await api.get(route).set("Authorization", `Bearer ${token}`);
         expect(result.status).toEqual(200);
         expect(result.body).toBeInstanceOf(Array)
     });
+
+    it("return status code 401 when using invalid token", async () => {
+        const result = await api.get(route).set("Authorization", `Bearer invalid_token`);
+        expect(result.status).toEqual(401);
+    })
 
 })
